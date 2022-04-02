ecfa6adaefbe6e2797785d4b2033c87953fb0652
joaochioda <joao.chioda@dextra-sw.com>
2022-04-01 21:32:03 -0300

    docker and find by name

--- back-micro/src/controllers/UserController.js
+++ back-micro/src/controllers/UserController.js
@@ -1,15 +1,27 @@
-const User = require('../models/User')
+const User = require("../models/User");
 
 module.exports = {
-    async store(req, res) {
-        const { name } = req.body;
-        const user = await User.create({name})
+  async store(req, res) {
+    const { name } = req.body;
+    const user = await User.create({ name });
 
-        return res.json(user)
-    },
-    async index(req, res) {
-        const users = await User.findAll();
+    return res.json(user);
+  },
+  async index(req, res) {
+    const users = await User.findAll();
 
-        return res.json(users);
+    return res.json(users);
+  },
+  async show(req, res) {
+    try {
+      const { name } = req.params;
+      const user = await User.findOne({
+        where: { name },
+      });
+
+      return res.json(user);
+    } catch (error) {
+      return res.json(error);
     }
-}
+  },
+};
--- back-micro/src/routes.js
+++ back-micro/src/routes.js
@@ -7,7 +7,8 @@ routes.get("/", (req, res) => {
   return res.json("hello world server");
 });
 
-routes.post("/users", UserController.store);
+routes.post("/user", UserController.store);
 routes.get("/users", UserController.index);
+routes.get("/user/:name", UserController.show);
 
 module.exports = routes;
