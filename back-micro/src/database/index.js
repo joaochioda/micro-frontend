f3f606350ced5d173eaed948de983a9ab34560d2
joaochioda <joao.chioda@dextra-sw.com>
2022-04-01 21:28:58 -0300

    docker running

--- back-micro/src/database/index.js
+++ back-micro/src/database/index.js
@@ -1,10 +1,10 @@
-const Sequelize = require('sequelize')
-const dbConfig = require('../config/database')
+const Sequelize = require("sequelize");
+const dbConfig = require("../config/database");
 
-const User = require('../models/User')
+const User = require("../models/User");
 
-const connection = new Sequelize(dbConfig)
+const connection = new Sequelize(dbConfig);
 
-User.init(connection)
+User.init(connection);
 
 module.exports = connection;
--- back-micro/src/routes.js
+++ back-micro/src/routes.js
@@ -1,14 +1,13 @@
-const express = require('express')
-const routes  = express.Router();
+const express = require("express");
+const routes = express.Router();
 
-const UserController = require('./controllers/UserController');
+const UserController = require("./controllers/UserController");
 
-routes.get('/', (req,res)=> {
-    return res.json('ola mundo')
-})
+routes.get("/", (req, res) => {
+  return res.json("hello world server");
+});
 
-routes.post('/users', UserController.store)
-routes.get('/users', UserController.index)
-
+routes.post("/users", UserController.store);
+routes.get("/users", UserController.index);
 
 module.exports = routes;
